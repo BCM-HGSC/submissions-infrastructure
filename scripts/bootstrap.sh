4e5c7ba6fa8912d22a0e021e5b78d125bd434a75
Walker Hale IV <walker.hale.iv@gmail.com>
2022-09-07 00:29:22 -0500

    wip

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -8,12 +8,21 @@ main() {
 
     trap cleanup SIGINT SIGTERM ERR EXIT
 
+    if [[ -n $verbose ]]; then
+        set -x
+    fi
+
     msg "${BLUE}Read parameters:${NOFORMAT}"
     msg "- force: ${force}"
     msg "- target_dir: ${target_dir}"
+    msg
 
     # script logic here
+
+    setup_temp
+
     ensure_conda
+    msg "Haz conda?"
 }
 
 usage() {
@@ -29,8 +38,8 @@ Available options:
 
 -h, --help      Print this help and exit.
 -v, --verbose   Print script debug info.
---force         Force overwriting existing infrastructure!
 --no-color      Turn off color output.
+--force         Force overwriting existing infrastructure!
 EOF
     exit
 }
@@ -40,12 +49,13 @@ parse_params() {
 
     # default values of variables set from params
     force=
+    verbose=
 
     while :; do
         case "${1-}" in
             -h | --help) usage ;;
-            -v | --verbose) set -x ;;
-            --no-color) NO_COLOR=1 ;;
+            -v | --verbose) verbose=y ;;
+            --no-color) NO_COLOR=y ;;
             --force) force=1 ;;
             -?*) die "Unknown option: $1 (-h for help)" ;;
             *) break ;;
@@ -62,24 +72,43 @@ parse_params() {
     return 0
 }
 
+setup_temp() {
+    tmp_root=$(dirname $(mktemp -u))
+    echo "tmp_root=$tmp_root"
+    user_root=$tmp_root/$USER
+    echo "user_root=$user_root"
+    mkdir -p $user_root
+    template=$tmp_root/$USER/$(date +%Y-%m-%d)-XXXX
+    my_tmp_dir=$(mktemp -d $template)
+    echo "my_tmp_dir=$my_tmp_dir"
+    cd $my_tmp_dir
+}
+
 ensure_conda() {
+    echo hello
     if [[ ! -x ${CONDA-} ]]; then
-        CONDA=$(which conda)
+        CONDA=$(which conda) || msg "No conda in PATH."
     fi
+
+    echo foo
 
     if [[ ! -x $CONDA ]]; then
         # What do you do when you have no conda?
         # What do you do when you have no conda?
         # What do you do when you have no conda?
         # Fetch it from the server!
-        msg fetch conda!!!
+        msg "fetch conda!!!"
     fi
 }
 
 cleanup() {
     trap - SIGINT SIGTERM ERR EXIT
     # script cleanup here
-    msg ${BLUE}CLEANUP
+    cd
+    msg ${BLUE}CLEANUP${NOFORMAT}
+    msg "rm -rf $user_root"
+    rm -rf "$user_root"
+    msg ${BLUE}DONE${NOFORMAT}
 }
 
 msg() {

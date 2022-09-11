ebc2d62277721675b076d2110a3b1fe883d41041
Walker Hale IV <walker.hale.iv@gmail.com>
2022-09-11 15:52:05 -0500

    Significant updates
    - Introduce bootstrap_engine.py.
    - Add --keep option.
    - Convert exportable shell variables to upper case.
    - Enhance logging.
    - Reorder steps in main() bash function.
    - Enhance logic of get_conda() bash function.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -11,6 +11,9 @@ subdirectory named "conda_package_cache" and another subdirectory named
 "infrastructure". It is an error if "infrastructure" already exists unless
 the --force option is applied.
 
+Following the creation of the infrastructure directory, this script will create
+engine_home if it does not already exist and then install or update the engine.
+
 Available options:
 
 -h, --help          Print this help and exit.
@@ -18,7 +21,15 @@ Available options:
 --no-color          Turn off color output.
 --force             Force overwriting existing infrastructure!
 --offline           No internet usage
+-k, --keep          Keep and use current deployment engine
 -n, --no-installs   No conda or pip installs, just skeleton and condarc
+
+Environment:
+CONDA: optional path to a conda executable
+PATH: will be searched for "conda" if CONDA is not set
+
+If a conda executable is not set by either PATH or CONDA, this script will
+download the latest version of Miniconda3 into a temporary directory and use that.
 EOF
     exit
 }
@@ -30,64 +41,71 @@ main() {
 
     trap cleanup SIGINT SIGTERM ERR EXIT
 
-    if [[ $verbose == 'yy' ]]; then
+    if [[ $VERBOSE == 'yy' ]]; then
         set -x
     fi
 
-    msg "${BLUE}Read parameters:${NOFORMAT}"
-    dump_var verbose
-    dump_var force
-    dump_var offline
-    dump_var no_installs
-    dump_var target_dir
+    info "${BLUE}Read parameters:${NOFORMAT}"
+    dump_vars VERBOSE FORCE OFFLINE KEEP NO_INSTALLS TARGET_DIR
     msg
 
-    script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
-    dump_var script_dir
-
-    # script logic here
-    get_conda
-    export PATH=/usr/bin:/bin
-
-    mkdir -p "$target_dir"
-    resolved_target=$(cd -P "$target_dir"; pwd)
-    dump_var resolved_target
+    SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+    RESOURCES_DIR=$(cd $SCRIPT_DIR/../resources && pwd -P)
+    mkdir -p "$TARGET_DIR"
+    RESOLVED_TARGET=$(cd -P "$TARGET_DIR"; pwd)
+    dump_vars SCRIPT_DIR RESOURCES_DIR RESOLVED_TARGET
 
     setup_target
+ 
+    get_conda
 
-    export CONDA_ENVS_DIRS="$resolved_target/infrastructure/current/conda/envs"
-    export CONDA_PKGS_DIRS=$resolved_target/conda_package_cache
-    export HOME=$resolved_target
-    export CONDARC=$resolved_target/condarc
-    if [[ $verbose == 'y' && -n $CONDA ]]; then
+    # Environment variables used by conda when creating the engine.
+    export CONDA_ENVS_DIRS="$RESOLVED_TARGET/infrastructure/current/conda/envs"
+    export CONDA_PKGS_DIRS="$RESOLVED_TARGET"/conda_package_cache
+    export HOME="$RESOLVED_TARGET"/engine_home
+    export CONDARC="$RESOLVED_TARGET"/condarc
+
+    use_miniconda3_in_temp_for_conda_if_necessary
+
+    if [[ -z $KEEP ]]; then
+        deploy_engine
+    fi
+
+    msg
+    info "New values"
+    CONDA="$HOME"/engine/bin/conda
+    PYTHON="$HOME"/engine/bin/python3
+    dump_vars CONDA PYTHON
+
+    if [[ $VERBOSE == 'y' && -x $CONDA ]]; then
+        ls -ld "$CONDA"
         "$CONDA" info
     fi
 
-    if [[ -n $no_installs ]]; then
+    if [[ -n $NO_INSTALLS ]]; then
         exit
     fi
 
-    use_miniconda3_in_temp_for_conda_if_necessary
-    deploy_engine
+    warning "TODO add team environments"
 } >&2
 
 parse_params() {
-    # Set force and target_dir.
-
     # default values of variables set from params
-    verbose=
-    force=
-    offline=
-    no_installs=
+    VERBOSE=
+    FORCE=
+    OFFLINE=
+    KEEP=
+    NO_INSTALLS=
 
     while :; do
         case "${1-}" in
             -h | --help) usage ;;
-            -v | --verbose) verbose=${verbose}y ;;
+            -v | --verbose) VERBOSE=${VERBOSE}y ;;
             --no-color) NO_COLOR=y ;;
-            --force) force=y ;;
-            --offline) offline=y ;;
-            -n | --no-installs) no_installs=y ;;
+            --force) FORCE=y ;;
+            --offline) OFFLINE=y ;;
+            -k | --keep) KEEP=y ;;
+            -n | --no-installs) NO_INSTALLS=y ;;
             -?*) die "Unknown option: $1 (-h for help)" ;;
             *) break ;;
         esac
@@ -95,27 +113,27 @@ parse_params() {
     done
 
     [[ $# -eq 0 ]] && die "missing value for TARGET_DIR (-h for help)"
-    target_dir="$1"
+    TARGET_DIR="$1"
     shift
 
     [[ $# -eq 0 ]] || die "unused positional parameters: $@ (-h for help)"
 
-    return 0
+    return
 }
 
 setup_target() {
-    cd "$resolved_target"
+    cd "$RESOLVED_TARGET"
     if [[ -e infrastructure ]]; then
-        if [[ -z $force ]]; then
-            die "$target_dir/infrastructure already exists"
+        if [[ -z $FORCE ]]; then
+            die "$TARGET_DIR/infrastructure already exists"
         else
-            msg "overwriting $resolved_target/infrastructure"
+            warning "overwriting $RESOLVED_TARGET/infrastructure"
             rm -rf infrastructure
         fi
     fi
-    mkdir -p conda_package_cache infrastructure user_envs
-    if ! ls -ld $resolved_target/condarc; then
-        msg 'Creating condarc symlink'
+    mkdir -p conda_package_cache engine_home infrastructure user_envs
+    if ! ls $RESOLVED_TARGET/condarc &> /dev/null; then
+        info 'Creating condarc symlink'
         ln -s infrastructure/current/condarc
     fi
     cd infrastructure
@@ -126,23 +144,36 @@ setup_target() {
 }
 
 write_condarc() {
-    local condarc_template=$script_dir/../resources/condarc.m4
-    m4 -D RESOLVED_TARGET=$resolved_target $condarc_template > current/condarc
+    local condarc_template=$RESOURCES_DIR/condarc.m4
+    m4 -D RESOLVED_TARGET=$RESOLVED_TARGET $condarc_template > current/condarc
 }
 
 get_conda() {
     # Set CONDA if not set and available in PATH.
-    : ${CONDA:=$(which conda)}  # Set CONDA if not set to conda in PATH
-    if [[ ! -x $CONDA ]]; then
-        warning "CONDA is not set, and no conda executable found in PATH."
+    if [[ ! -x "${CONDA:=}" ]]; then
+        if [[ -n "$CONDA" ]]; then
+            warning "CONDA=$CONDA"
+            warning "Supplied vaule of CONDA is not executable."
+        fi
+        CONDA=$(command -v conda || :)  # will be "" if not found in PATH
+        if [[ -n "$CONDA" ]]; then
+            if [[ ! -x "$CONDA" ]]; then
+                warning "CONDA from PATH is not executable."
+                warning "CONDA=$CONDA (bad value in PATH)"
+            else
+                dump_var CONDA
+            fi
+        else
+            warning "No conda in PATH."
+            warning "PATH=$PATH"
+        fi
     fi
-    dump_var CONDA
 }
 
 use_miniconda3_in_temp_for_conda_if_necessary() {
     # If CONDA is unset, ownload and install Miniconda3 in order to set CONDA.
     if [[ ! -x ${CONDA-} ]]; then
-        if [[ -n $offline ]]; then
+        if [[ -n $OFFLINE ]]; then
             error "CONDA is not set, and we are offline"
             return
         fi
@@ -152,10 +183,10 @@ use_miniconda3_in_temp_for_conda_if_necessary() {
         # Fetch it from the server!
         setup_temp
         check_os
-        msg "Fetching conda..."
+        info "Fetching conda..."
         cd $my_tmp_dir
         url="https://repo.anaconda.com/miniconda/Miniconda3-latest-$plat-x86_64.sh"
-        msg "fetching $url"
+        info "fetching $url"
         curl "$url" -o installer.sh
         bash installer.sh -bp ./miniconda
         CONDA=$my_tmp_dir/miniconda/condabin/conda
@@ -165,13 +196,13 @@ use_miniconda3_in_temp_for_conda_if_necessary() {
 
 setup_temp() {
     tmp_root=$(dirname $(mktemp -u))
-    # msg "tmp_root=$tmp_root"
+    debug "tmp_root=$tmp_root"
     user_root=$tmp_root/$USER
-    # msg "user_root=$user_root"
+    debug "user_root=$user_root"
     mkdir -p $user_root
     template=$tmp_root/$USER/$(date +%Y-%m-%d)-XXXX
     my_tmp_dir=$(mktemp -d $template)
-    # msg "my_tmp_dir=$my_tmp_dir"
+    debug "my_tmp_dir=$my_tmp_dir"
 }
 
 check_os() {
@@ -184,37 +215,43 @@ check_os() {
 }
 
 deploy_engine() {
-    if [[ ! -x $CONDA ]]; then
-        error "Cannot deploy engine, because there is no conda"
-        return
-    fi
-    msg deploy_engine
-    engine_path=$resolved_target/engine
-    rm -rf $engine_path
-    local offline_opt=
-    [[ -n $offline ]] && offline_opt='--offline'
-    $CONDA create $offline_opt -y -p $engine_path conda pip
-    export PATH=$engine_path/bin:$PATH
-    dump_var PATH
-    which conda
+    [[ -x $CONDA ]] || die "Cannot deploy engine, because there is no conda"
+    dump_var HOME
+    PYTHON="$($CONDA info --base)"/bin/python3
+    dump_var PYTHON
+    export CONDA OFFLINE VERBOSE
+    cd "$HOME"
+    msg
+    run_python bootstrap_engine.py
+}
+
+run_python() {
+    info "run_python $@"
+    local script_name=$1
+    shift
+    "$PYTHON" "$SCRIPT_DIR"/$script_name "$@"
 }
 
 cleanup() {
     trap - SIGINT SIGTERM ERR EXIT
     # script cleanup here
     cd
-    msg ${BLUE}CLEANUP${NOFORMAT}
+    info ${BLUE}CLEANUP${NOFORMAT}
     if [[ -e ${my_tmp_dir-} ]]; then
-        msg "rm -rf $my_tmp_dir"
+        info "rm -rf $my_tmp_dir"
         rm -rf "$my_tmp_dir"
     fi
-    msg ${BLUE}DONE${NOFORMAT}
+    info ${BLUE}DONE${NOFORMAT}
 } >&2
+
+dump_vars() {
+    for name in $@; do dump_var $name; done
+}
 
 dump_var() {
     local var_name=$1
     local value="${!var_name}"
-    msg "$var_name=$value"
+    info "$var_name=$value"
 }
 
 die() {
@@ -234,6 +271,16 @@ error() {
 
 warning() {
     msg "${ORANGE}WARNING${NOFORMAT}: $1"
+}
+
+info() {
+    msg "${BLUE}INFO${NOFORMAT}: $1"
+}
+
+debug() {
+    if [[ -n $VERBOSE ]]; then
+        msg "${GREEN}DEBUG${NOFORMAT}: $1"
+    fi
 }
 
 msg() {
---
+++ scripts/bootstrap_engine.py
@@ -0,0 +1,89 @@
+"""
+Python script to bootstrap or rotate the infrastructure maintenance angine.
+
+This script takes no command-line parameters.
+
+Required environment variables:
+CONDA: location of the conda executable
+CONDARC: location of the condarc configuration file
+
+Note that this variable is set from the current working directory:
+HOME: directory used by conda as the "home directory" for configuration
+
+Optional environment variables:
+OFFLINE: if set run conda create with the --offline option
+VERBOSE: if set, logging level is DEBUG, otherwise INFO
+"""
+
+from logging import DEBUG, INFO, StreamHandler, basicConfig, error, critical, info, debug
+from os import environ
+from pathlib import Path
+from shutil import rmtree
+from subprocess import run
+from sys import argv, executable, exit, stderr
+
+try:
+    from rich.logging import RichHandler
+    ROOT_HANDLER = RichHandler(show_time=False)
+    ROOT_FORMAT = "%(message)s"
+except:
+    ROOT_HANDLER = StreamHandler()
+    ROOT_FORMAT = "%(levelname)s: %(message)s"
+
+
+def main(run_function=run):
+    config_logging()
+    info(f"starting {__file__}")
+    if len(argv) > 1:
+        critical(f"unknown command-line parameters: {argv[1:]}")
+        return 2
+    home = Path().resolve()
+    executable_path = Path(executable)
+    conda = env_path("CONDA")
+    engine_path = rotate_engine_directories(home, executable_path, conda)
+    symlink = home / "engine"
+    conda_opts = ["--offline"] if environ["OFFLINE"] else []
+    debug(f"{conda_opts=}")
+    conda_command = [conda, "create"] + conda_opts
+    conda_command +=  f"-y -p {engine_path} conda pip rich".split()
+    info(f"{conda_command=}")
+    env = dict(
+        HOME=home,
+        CONDARC=env_path("CONDARC"),
+    )
+    info(f"{env=}")
+    run_function(conda_command, check=True, env=env)
+    if symlink.is_symlink():
+        symlink.unlink()
+    symlink.symlink_to(engine_path)
+
+
+def config_logging():
+    level = DEBUG if environ["VERBOSE"] else INFO
+    basicConfig(level=level, format=ROOT_FORMAT, handlers=[ROOT_HANDLER])
+
+
+def env_path(key: str) -> Path:
+    if key not in environ:
+        raise KeyError(f"missing environment variable {key}")
+    return Path(environ[key]).resolve()
+
+
+def rotate_engine_directories(home: Path, executable_path: Path, conda: Path) -> Path:
+    """
+    Clean out unused directories and select the directory to use next.
+    """
+    engine_path = None
+    for letter in "abc":
+        possible_path = home / f"engine_{letter}"
+        conflicts_executable = executable_path.is_relative_to(possible_path)
+        conflicts_conda = conda.is_relative_to(possible_path)
+        if not (conflicts_executable or conflicts_conda):
+            rmtree(possible_path, ignore_errors=True)
+            if not possible_path.exists():
+                engine_path = engine_path or possible_path
+    return engine_path
+
+
+if __name__ == "__main__":
+    exit(main())

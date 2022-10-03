6637c2828ad40dd7a31f63ef17cf0fd674bd465e
Walker Hale IV <walker.hale.iv@gmail.com>
2022-10-03 14:04:46 -0500

    wip profile

--- resources/etc/profile.sh
+++ resources/etc/profile.sh
@@ -1,6 +1,25 @@
-# TODO: render
+# Top-level RC file to be sourced by the team for interactive sessions.
+# Sources other scripts and sets environment variables.
 
-for i in {profile_dir}/*.sh ; do
+
+if [[ -z $TIER_DIR ]]; then
+    if [[ $ZSH_NAME ]]; then
+        __temp_arg0="$0"
+    fi
+    if [[ $BASH ]]; then
+        __temp_arg0="${BASH_SOURCE[0]}"
+    fi
+    # echo __temp_arg0=$__temp_arg0
+    __temp_this_script=$(/usr/bin/readlink -f "$__temp_arg0")
+    # echo __temp_this_script=$__temp_this_script
+    TIER_DIR=$(/usr/bin/dirname $(/usr/bin/dirname "$__temp_this_script"))
+    unset __temp_arg0 __temp_this_script
+fi
+export TIER_DIR
+
+echo "TIER_DIR=$TIER_DIR"
+
+for i in "$TIER_DIR"/etc/profile.d/*.sh ; do
     if [ -r "$i" ]; then
         . "$i"
     fi

657c71099f641e4579f0b4aa49eef5b170bb7ee0
Walker Hale IV <walker.hale.iv@gmail.com>
2022-11-01 14:17:10 -0500

    Update profile.sh
    - Remove conditional logic on IAC_TIER_DIR.
    - Add iac_dump_vars.

--- resources/etc/profile.sh
+++ resources/etc/profile.sh
@@ -3,20 +3,20 @@
 
 # IAC_* is an abbreviation for infrastructure-as-code.
 
-if [[ -z $IAC_TIER_DIR ]]; then
-    if [[ $ZSH_NAME ]]; then
-        __temp_arg0="$0"
-    fi
-    if [[ $BASH ]]; then
-        __temp_arg0="${BASH_SOURCE[0]}"
-    fi
-    # echo __temp_arg0=$__temp_arg0
-    __temp_this_script=$(/usr/bin/readlink -f "$__temp_arg0")
-    # echo __temp_this_script=$__temp_this_script
-    IAC_TIER_DIR=$(/usr/bin/dirname $(/usr/bin/dirname "$__temp_this_script"))
-    unset __temp_arg0 __temp_this_script
+if [[ $ZSH_NAME ]]; then
+    __temp_arg0="$0"
 fi
-export IAC_TIER_DIR
+if [[ $BASH ]]; then
+    __temp_arg0="${BASH_SOURCE[0]}"
+fi
+
+__temp_this_script=$(/usr/bin/readlink -f "$__temp_arg0")
+
+export IAC_TIER_DIR=$(/usr/bin/dirname $(/usr/bin/dirname "$__temp_this_script"))
+export IAC_DIR=$(/usr/bin/dirname "$IAC_TIER_DIR")
+export IAC_TIER_NAME=$(/usr/bin/basename $(/usr/bin/dirname $(/usr/bin/dirname "$__temp_arg0")))
+
+unset __temp_arg0 __temp_this_script
 
 if [[ -z ${IAC_ORIGINAL_PATH:=$PATH} ]]; then
     echo "WARNING: original PATH is empty!"
@@ -47,11 +47,16 @@ source "$__CONDA_ENVS_DIR/conda/etc/profile.d/mamba.sh"
 unset __add_env_if_exists
 unset __CONDA_ENVS_DIR
 
-export IAC_DIR=$(/usr/bin/dirname "$IAC_TIER_DIR")
+iac_dump_vars() {
+    echo __temp_arg0=$__temp_arg0
+    echo __temp_this_script=$__temp_this_script
+    echo "IAC_TIER_DIR=$IAC_TIER_DIR"
+    echo "IAC_TIER_NAME=$IAC_TIER_NAME"
+    echo "IAC_DIR=$IAC_DIR"
+}
 
 if [[ -n $VERBOSE ]]; then
-    echo "IAC_TIER_DIR=$IAC_TIER_DIR"
-    echo "IAC_DIR=$IAC_DIR"
+    iac_dump_vars
 fi
 
 PATH=~/bin:"$IAC_DIR/bin":"$IAC_TIER_DIR/bin":"$PATH"
@@ -76,6 +81,8 @@ fi
 iac_load() {
     local tier="$1"
     shift
+    IAC_TIER_DIR=
+    IAC_TIER_NAME=
     source "$IAC_DIR/$tier/etc/profile.sh"
 }
 

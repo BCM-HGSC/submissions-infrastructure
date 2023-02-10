1a2f0f84b2097ec48ac5fd5b8822d7b2898a725a
Walker Hale IV <walker.hale.iv@gmail.com>
2023-02-10 10:50:13 -0600

    Remove interactive_variables.sh
    This was only useful on laptops with VS Code installed along with
    a user custom script in $HOME/bin/code-wait. These should be user
    settings.

--- resources/etc/profile.d/interactive_variables.sh
+++
@@ -1,5 +0,0 @@
-#export EDITOR=emacs
-# export EDITOR="/usr/local/bin/code -w"
-export EDITOR=~/bin/code-wait
-# export LESSEDIT='mate -l %lm %f'
-export LESSEDIT="/usr/local/bin/code -w --goto '%f\:%lm'"

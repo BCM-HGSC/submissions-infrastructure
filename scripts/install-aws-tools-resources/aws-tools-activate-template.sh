d9c523e5a3db4e5dcc6b6846579a48281ff70968
Walker Hale IV <walker.hale.iv@gmail.com>
2023-07-18 13:49:47 -0700

    Support non-interactive shells with 'set -eu'
    PS1 is undefined and accessing an undefined variable is an error.

--- scripts/install-aws-tools-resources/aws-tools-activate-template.sh
+++ scripts/install-aws-tools-resources/aws-tools-activate-template.sh
@@ -1,14 +1,14 @@
 # Source this file to activate the HGSC aws toolkit.
 
 __HGSC_AWS_TOOLS_OLD_PATH=$PATH
-__HGSC_AWS_TOOLS_OLD_PS1=$PS1
+__HGSC_AWS_TOOLS_OLD_PS1=${PS1:-}
 
 export PATH="PREFIX_DIRS:$PATH"
-PS1="(aws) $PS1"
+[[ -n "$__HGSC_AWS_TOOLS_OLD_PS1" ]] && PS1="(aws) $PS1"
 
 __aws_tools_deactivate() {
     export PATH="$__HGSC_AWS_TOOLS_OLD_PATH"
-    PS1="$__HGSC_AWS_TOOLS_OLD_PS1"
+    [[ -n "$__HGSC_AWS_TOOLS_OLD_PS1" ]] && PS1="$__HGSC_AWS_TOOLS_OLD_PS1"
     unset __HGSC_AWS_TOOLS_OLD_PATH __HGSC_AWS_TOOLS_OLD_PS1
     unalias deactivate
     unset -f __aws_tools_deactivate
